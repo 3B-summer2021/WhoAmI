ec2092049e3b9d2ac0a5ae15e6458d1eaf425836
Nayoung-Oh <lightsalt540@gmail.com>
2021-08-25 23:10:06 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
-import { Route, BrowserRouter, Switch } from "react-router-dom";
+import { Route, BrowserRouter, Switch, Redirect } from "react-router-dom";
 import Home from "./container/Home";
 import Question from "./container/Question";
 import Ending from "./container/Ending";
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Switch>
-        <Route path="/" exact render={() => <Home />} />
-        <Route path="/question/:id" exact render={() => <Question />} />
-        <Route path="/ending" exact render={() => <Ending />} />
+        <Route path="/" exact component={Home} />
+        <Route path="/question/:id" exact component={Question} />
+        <Route path="/ending" exact component={Ending} />
+        <Redirect from="*" to="/" />
       </Switch>
     </BrowserRouter>
   );
