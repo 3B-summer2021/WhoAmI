021558901d1a468c2427d2cd145d1096ce309186
Nayoung-Oh <lightsalt540@gmail.com>
2021-08-25 23:41:01 +0900

    router final

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
-import { Route, HashRouter, Switch, Redirect } from "react-router-dom";
+import { Route, HashRouter, Switch } from "react-router-dom";
 import Home from "./container/Home";
 import Question from "./container/Question";
 import Ending from "./container/Ending";
 function App() {
   return (
     <HashRouter basename={process.env.PUBLIC_URL}>
-      <Route path="/" component={Home} exact />
-      <Route path="/question/:id" component={Question} />
-      <Route path="/ending" component={Ending} />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/question/:id" component={Question} />
+        <Route path="/ending" component={Ending} />
+      </Switch>
     </HashRouter>
   );
 }
--- src/container/Question.js
+++ src/container/Question.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Question() {
-  return <div>Question</div>;
+function Qeustion({ match }) {
+  return <div>Qeustion {match.params.id}</div>;
 }
 
-export default Question;
+export default Qeustion;
