dcf989a26effd8aa19b5745faac331cc81c6b6cd
Nayoung-Oh <lightsalt540@gmail.com>
2021-08-25 22:32:31 +0900

    tmp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
-import { Route, BrowserRouter } from "react-router-dom";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
 import Home from "./container/Home";
 import Question from "./container/Question";
 import Ending from "./container/Ending";
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Route path="/" component={Home} exact />
-      <Route path="/question/:id" component={Question} />
-      <Route path="/ending" component={Ending} />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/question/:id" component={Question} />
+        <Route path="/ending" component={Ending} />
+      </Switch>
     </BrowserRouter>
   );
 }
