375a7efc647eb586dd05479f17e39263af4c0a51
darami14 <bbvilee1214@gmail.com>
2021-08-29 15:51:33 +0900

    question

--- src/container/Question.js
+++ src/container/Question.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import Answer from "../components/question/answer";
+import q_list from "../components/question/question_list";
 function Question(props) {
   const id = parseInt(useParams().id);
   // console.log(id);
+
   return (
     <div>
-      Qusetion {id}
+      Question {id} {q_list[id-1]}
       <Answer answer={props.answer} setAnswer={props.setAnswer} id={id} />
     </div>
   );
