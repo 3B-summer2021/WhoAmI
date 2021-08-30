87140951fb2f10d6eef1165871b699a835554e16
darami14 <bbvilee1214@gmail.com>
2021-08-30 21:13:48 +0900

    state를 모르겠어요

--- src/components/question/answer.js
+++ src/components/question/answer.js
@@ -3,14 +3,12 @@ import { useHistory } from "react-router-dom";
 import a_list_1 from "./answer_list_1";
 import a_list_2 from "./answer_list_2";
 
-var e_list = []
-
 function Answer({ answer, setAnswer, id }) {
   const history = useHistory();
   const NextPage = () => {
     if (id < 8) {
       const len = id + 1;
-      setAnswer(answer.concat("hi"));
+      /*setAnswer(answer.concat(0));*/
       history.push("/question/" + len);
     } else {
       history.push("/ending");
@@ -18,13 +16,12 @@ function Answer({ answer, setAnswer, id }) {
   };
   // console.log(id);
   return (
-    <div>
-      <button onClick={() => {NextPage(); e_list.push("T");}}>{a_list_1[id-1]}</button>
-      <button onClick={() => {NextPage(); e_list.push("F");}}>{a_list_2[id-1]}</button>
-    </div>
+      <div>
+        <button onClick={() => {NextPage(); setAnswer(answer.concat(0));}}>{a_list_1[id-1]}</button>
+        <button onClick={() => {NextPage(); setAnswer(answer.concat(1));}}>{a_list_2[id-1]}</button>
+      </div>
   );
 }
 
+console.log(answer)
 export default Answer;
-
-console.log(e_list)
